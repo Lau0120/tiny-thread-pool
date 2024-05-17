3f4d3400820ee6df181f9c154ad67fd6b0b2df88
Hell Wander <yingxinlau0120@gmail.com>
2024-05-17 14:04:47 +0800

    update tiny_tp.hpp

--- tiny_tp.hpp
+++ tiny_tp.hpp
@@ -36,25 +36,72 @@ class ThreadPool {
   /// maximum queue size.
   /// @param num_threads The number of threads in the `ThreadPool`.
   /// @param max_queue_size The maximum size of the task queue.
-  ThreadPool(std::uint32_t num_threads, std::uint32_t max_queue_size);
+  ThreadPool(std::uint32_t num_threads, std::uint32_t max_queue_size)
+      : kNumThreads{num_threads}, kMaxQueueSize{max_queue_size},
+        exit_mark_{new std::uint32_t{0}} {
+    std::lock_guard<std::mutex> threads_idle_guard(threads_idle_mtx_);
+    // Create the specified number of threads and start them.
+    for (std::uint32_t i = 0; i < kNumThreads; ++i) {
+      auto thread = std::thread(&ThreadPool::Cycle, this);
+      threads_idle_[thread.get_id()] = true;
+      thread.detach();
+    }
+  }
   ThreadPool()
       : ThreadPool{std::thread::hardware_concurrency(), kDefaultMaxQueueSize} {}
   explicit ThreadPool(std::uint32_t num_threads)
       : ThreadPool{num_threads, kDefaultMaxQueueSize} {};
-  ~ThreadPool();
+  ~ThreadPool() {
+    // Terminate threads one by one.
+    for (std::uint32_t i = 0; i < kNumThreads; ++i) {
+      // Send quit "signal" to each thread.
+      Drop(std::make_shared<QuitTask>(exit_mark_));
+      while (*exit_mark_ != i + 1) {
+        // Reschedule execution of threads (lower the priority of main thread)
+        std::this_thread::yield();
+      }
+    }
+  }
 
   /// @brief Adds a task to the `ThreadPool` queue (non-blocking).
   /// @param task The task to be added to the queue.
   /// @return True if the task was successfully added, false otherwise.
-  bool Drop(std::shared_ptr<ITask> task);
+  bool Drop(std::shared_ptr<ITask> task) {
+    std::lock_guard<std::mutex> waiting_queue_guard{waiting_queue_mtx_};
+    if (waiting_queue_.size() >= kMaxQueueSize) {
+      return false;
+    }
+    waiting_queue_.push(task);
+    waiting_queue_cond_.notify_one();
+    return true;
+  }
   /// @brief Retrieves all results from the `ThreadPool` queue (non-blocking).
   /// @return A vector containing all results from executed tasks.
-  std::vector<std::shared_ptr<void>> GrabAllResults();
-  std::uint32_t max_queue_size() const { return kMaxQueueSize; }
-  std::uint32_t num_threads() const { return kNumThreads; }
-  size_t QueryIdleThreadsCount();
-  size_t QueryWaitingQueueCount();
-  size_t QueryResultsCount();
+  std::vector<std::shared_ptr<void>> GrabAllResults() {
+    std::vector<std::shared_ptr<void>> results;
+    std::lock_guard<std::mutex> results_queue_guard{results_queue_mtx_};
+    while (!results_queue_.empty()) {
+      results.push_back(results_queue_.front());
+      results_queue_.pop();
+    }
+    return results;
+  }
+  [[nodiscard]] std::uint32_t max_queue_size() const { return kMaxQueueSize; }
+  [[nodiscard]] std::uint32_t num_threads() const { return kNumThreads; }
+  size_t QueryIdleThreadsCount() {
+    std::lock_guard<std::mutex> threads_idle_guard{threads_idle_mtx_};
+    return std::count_if(
+        threads_idle_.cbegin(), threads_idle_.cend(),
+        [](const std::pair<std::thread::id, bool>& e) { return e.second; });
+  }
+  size_t QueryWaitingQueueCount() {
+    std::lock_guard<std::mutex> waiting_queue_guard{waiting_queue_mtx_};
+    return waiting_queue_.size();
+  }
+  size_t QueryResultsCount() {
+    std::lock_guard<std::mutex> results_queue_guard{results_queue_mtx_};
+    return results_queue_.size();
+  }
 
  private:
   /// @brief A special task used to signal thread termination.
@@ -68,7 +115,39 @@ class ThreadPool {
   };
 
   /// @brief Main function for thread pool execution cycle.
-  void Cycle();
+  void Cycle() {
+    // Main loop for each thread.
+    while (true) {
+      // Try to take a task from queue.
+      std::unique_lock<std::mutex> waiting_queue_unique_lock{waiting_queue_mtx_};
+      // Keep waiting until the queue is not empty.
+      waiting_queue_cond_.wait(waiting_queue_unique_lock,
+                               [this]() { return !waiting_queue_.empty(); });
+      // Update thread to non-idle state.
+      std::unique_lock<std::mutex> threads_idle_unique_lock{threads_idle_mtx_};
+      threads_idle_[std::this_thread::get_id()] = false;
+      threads_idle_unique_lock.unlock();
+
+      std::shared_ptr<ITask> task = waiting_queue_.front();
+      waiting_queue_.pop();
+      waiting_queue_unique_lock.unlock();
+
+      auto result = task->Execute();
+      // Process the result of the task execution
+      if (result != nullptr) {
+        if (result == exit_mark_) {
+          ++(*exit_mark_);
+          break;  // Exit the loop if the result is an exit flag
+        }
+        std::lock_guard<std::mutex> results_guard{results_queue_mtx_};
+        results_queue_.push(result);
+      }
+
+      // Update thread to idle state.
+      threads_idle_unique_lock.lock();
+      threads_idle_[std::this_thread::get_id()] = true;
+    }
+  }
 
   std::queue<std::shared_ptr<ITask>> waiting_queue_;
   std::mutex waiting_queue_mtx_;
@@ -84,106 +163,6 @@ class ThreadPool {
 };
 
 constexpr std::uint32_t ThreadPool::kDefaultMaxQueueSize;
-
-}  // namespace tiny_tp
-
-namespace tiny_tp {  // implementations
-
-ThreadPool::ThreadPool(std::uint32_t num_threads, std::uint32_t max_queue_size)
-    : kNumThreads{num_threads},
-      kMaxQueueSize{max_queue_size},
-      exit_mark_{new std::uint32_t{0}} {
-  std::lock_guard<std::mutex> threads_idle_guard(threads_idle_mtx_);
-  // Create the specified number of threads and start them.
-  for (std::uint32_t i = 0; i < kNumThreads; ++i) {
-    auto thread = std::thread(&ThreadPool::Cycle, this);
-    threads_idle_[thread.get_id()] = true;
-    thread.detach();
-  }
-}
-
-ThreadPool::~ThreadPool() {
-  // Terminate threads one by one.
-  for (std::uint32_t i = 0; i < kNumThreads; ++i) {
-    // Send quit "signal" to each thread.
-    Drop(std::make_shared<QuitTask>(exit_mark_));
-    while (*exit_mark_ != i + 1) {
-      // Reschedule execution of threads (lower the priority of main thread)
-      std::this_thread::yield();
-    }
-  }
-}
-
-bool ThreadPool::Drop(std::shared_ptr<ITask> task) {
-  std::lock_guard<std::mutex> waiting_queue_guard{waiting_queue_mtx_};
-  if (waiting_queue_.size() >= kMaxQueueSize) {
-    return false;
-  }
-  waiting_queue_.push(task);
-  waiting_queue_cond_.notify_one();
-  return true;
-}
-
-std::vector<std::shared_ptr<void>> ThreadPool::GrabAllResults() {
-  std::vector<std::shared_ptr<void>> results;
-  std::lock_guard<std::mutex> results_queue_guard{results_queue_mtx_};
-  while (!results_queue_.empty()) {
-    results.push_back(results_queue_.front());
-    results_queue_.pop();
-  }
-  return results;
-}
-
-size_t ThreadPool::QueryIdleThreadsCount() {
-  std::lock_guard<std::mutex> threads_idle_guard{threads_idle_mtx_};
-  return std::count_if(
-      threads_idle_.cbegin(), threads_idle_.cend(),
-      [](const std::pair<std::thread::id, bool>& e) { return e.second; });
-}
-
-size_t ThreadPool::QueryWaitingQueueCount() {
-  std::lock_guard<std::mutex> waiting_queue_guard{waiting_queue_mtx_};
-  return waiting_queue_.size();
-}
-
-size_t ThreadPool::QueryResultsCount() {
-  std::lock_guard<std::mutex> results_queue_guard{results_queue_mtx_};
-  return results_queue_.size();
-}
-
-void ThreadPool::Cycle() {
-  // Main loop for each thread.
-  while (true) {
-    // Try to take a task from queue.
-    std::unique_lock<std::mutex> waiting_queue_unilock{waiting_queue_mtx_};
-    // Keep waiting untile the queue is not empty.
-    waiting_queue_cond_.wait(waiting_queue_unilock,
-                             [this]() { return !waiting_queue_.empty(); });
-    // Update thread to non-idle state.
-    std::unique_lock<std::mutex> threads_idle_unilock{threads_idle_mtx_};
-    threads_idle_[std::this_thread::get_id()] = false;
-    threads_idle_unilock.unlock();
-
-    std::shared_ptr<ITask> task = waiting_queue_.front();
-    waiting_queue_.pop();
-    waiting_queue_unilock.unlock();
-
-    auto result = task->Execute();
-    // Process the result of the task execution
-    if (result != nullptr) {
-      if (result == exit_mark_) {
-        ++(*exit_mark_);
-        break;  // Exit the loop if the result is an exit flag
-      }
-      std::lock_guard<std::mutex> results_guard{results_queue_mtx_};
-      results_queue_.push(result);
-    }
-
-    // Update thread to idle state.
-    threads_idle_unilock.lock();
-    threads_idle_[std::this_thread::get_id()] = true;
-  }
-}
 
 }  // namespace tiny_tp
 
